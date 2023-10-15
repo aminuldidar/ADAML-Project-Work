e1d9392ba44ce36b7b83cd91f5574a695b99ca2c
Bořek Reich <borek963@gmail.com>
2023-10-15 18:51:18 +0200

    PLS changes, optimization for different models added and optimized. Now, we need to add code for finding of the most useful variables with the best lags (Useful variables selection part).

--- model.py
+++ model.py
@@ -3,13 +3,17 @@ import numpy as np
 
 class Model:
     def __init__(self, model_params, model, scalers,
-                 train_mse_scores, train_f1_scores, train_r2_scores,
-                 valid_mse_scores, valid_f1_scores, valid_r2_scores):
+                 train_mse_scores=None, train_q2_scores=None, train_r2_scores=None,
+                 valid_mse_scores=None, valid_q2_scores=None, valid_r2_scores=None):
         self.model = model
         self.scalers = scalers
 
-        self.train_metrics = np.array([train_mse_scores, train_f1_scores, train_r2_scores])
-        self.valid_metrics = np.array([valid_mse_scores, valid_f1_scores, valid_r2_scores])
+        self.train_metrics = [train_mse_scores if train_mse_scores is not None else [],
+                              train_q2_scores if train_q2_scores is not None else [],
+                              train_r2_scores if train_r2_scores is not None else []]
+        self.valid_metrics = [valid_mse_scores if valid_mse_scores is not None else [],
+                              valid_q2_scores if valid_q2_scores is not None else [],
+                              valid_r2_scores if valid_r2_scores is not None else []]
 
         self.window_size = model_params[0]
         self.num_components = model_params[1]

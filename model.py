6f22a920defa44450eb94c37c45d8a27691b899d
Bořek Reich <borek963@gmail.com>
2023-10-16 15:44:38 +0200

    Final push before week 6 submission.

--- model.py
+++ model.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 class Model:
-    def __init__(self, model_params, model, scalers,
+    def __init__(self, model_params, model, scalers, centers=None,
                  train_mse_scores=None, train_q2_scores=None, train_r2_scores=None,
                  valid_mse_scores=None, valid_q2_scores=None, valid_r2_scores=None):
         self.model = model
@@ -14,6 +14,7 @@ class Model:
         self.valid_metrics = [valid_mse_scores if valid_mse_scores is not None else [],
                               valid_q2_scores if valid_q2_scores is not None else [],
                               valid_r2_scores if valid_r2_scores is not None else []]
+        self.y_centers = centers if centers is not None else []
 
         self.window_size = model_params[0]
         self.num_components = model_params[1]

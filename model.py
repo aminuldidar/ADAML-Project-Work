73dfdebd581eb867c79cb5ebd2ab63650b860aad
Borek Reich <borek963@gmail.com>
2023-10-21 16:12:02 +0200

    Variables with different lags

--- model.py
+++ model.py
@@ -1,19 +1,17 @@
-import numpy as np
-
 
 class Model:
     def __init__(self, model_params, model, scalers, centers=None,
                  train_mse_scores=None, train_q2_scores=None, train_r2_scores=None,
-                 test_mse_scores=None, test_q2_scores=None, test_r2_scores=None):
+                 valid_mse_scores=None, valid_q2_scores=None, valid_r2_scores=None):
         self.model = model
         self.scalers = scalers
 
         self.train_metrics = [train_mse_scores if train_mse_scores is not None else [],
                               train_q2_scores if train_q2_scores is not None else [],
                               train_r2_scores if train_r2_scores is not None else []]
-        self.test_metrics = [test_mse_scores if test_mse_scores is not None else [],
-                              test_q2_scores if test_q2_scores is not None else [],
-                              test_r2_scores if test_r2_scores is not None else []]
+        self.valid_metrics = [valid_mse_scores if valid_mse_scores is not None else [],
+                              valid_q2_scores if valid_q2_scores is not None else [],
+                              valid_r2_scores if valid_r2_scores is not None else []]
         self.y_centers = centers if centers is not None else []
 
         self.window_size = model_params[0]
